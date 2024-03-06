805c816c5983ac7701f7923202ecbe1d4e3735ca
Lucas <lucasmoniez@gmail.com>
2024-03-06 14:40:14 +0100

    LES PAGES

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { View } from "react-native";
-import Header from "./components/Accueil/Header";
-import Footer from "./components/Accueil/Footer";
-import Inscription from "./components/Authentification/Inscription";
+import React from 'react';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-// import Body from './components/Authentification/Body';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Header from './components/Accueil/Header' 
+import Footer from './components/Accueil/Footer';
+import Inscription from './components/Authentification/Inscription';
+import Home from "./screens/Home"
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <NavigationContainer>
-        <Header />
-        <Footer />
-        <Inscription />
+        <Stack.Navigator initialRouteName="Home" headerMode="none">
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+          <Stack.Screen name="Inscription" component={Inscription} />
+        </Stack.Navigator>
       </NavigationContainer>
     </View>
   );
