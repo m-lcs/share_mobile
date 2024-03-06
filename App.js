73d5e7f0c63590ed3f642f9e292e9d9460ef5ddc
Lucas <lucasmoniez@gmail.com>
2024-03-06 13:58:52 +0100

    correctif affichage

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import Header from './components/Accueil/Header';
 import Footer from './components/Accueil/Footer'
-import Body from './components/Authentification/Body';
+// import Body from './components/Authentification/Body';
 
 const App = () => {
   return (
     <View>
       <Header />
       <Footer />
-      <Body />
     </View>
   );
 }
--- components/Accueil/Header.jsx
+++ components/Accueil/Header.jsx
@@ -15,8 +15,8 @@ const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 40,
-    paddingBottom: -20,
+    paddingTop: 90,
+    paddingBottom: 20,
   },
   logo: {
     width: 120,
