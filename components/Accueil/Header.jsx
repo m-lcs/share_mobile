14d433823f2d3af9843ba422d4e2d6474be93875
Lucas <lucasmoniez@gmail.com>
2024-03-06 09:41:09 +0100

    on test

--- components/Accueil/Header.jsx
+++ components/Accueil/Header.jsx
@@ -1,1 +1,29 @@
-eeee
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>Share</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default Header;
