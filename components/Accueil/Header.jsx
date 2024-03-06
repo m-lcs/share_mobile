eb95edaa8ae1e3f515bb7e4cb474f035104f7f30
Lucas <lucasmoniez@gmail.com>
2024-03-06 09:58:00 +0100

    Update Header.jsx

--- components/Accueil/Header.jsx
+++ components/Accueil/Header.jsx
@@ -4,27 +4,31 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 const Header = () => {
   return (
     <View style={styles.header}>
-            <Image source={require('../../assets/share.png')} style={styles.logo} />
-      <Text style={styles.title}>Share</Text>
+      <Image source={require('../../assets/share.png')} style={styles.logo} />
+      <Text style={styles.titre}>Share</Text>
+      <Text style={styles.soustitre}>Partagez vos fichiers</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
   },
   logo: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
+    width: 100,
+    height: 100,
+    marginBottom: 10,
   },
-  title: {
+  titre: {
     fontSize: 24,
-    top: 30,
+    fontWeight: 'bold',
+  },
+  soustitre: {
+    fontSize: 18,
+    color: 'gray',
   },
 });
 
